2f99beefd278d2ed5ef25ca23116aaac75b0129a
Aluno <Aluno@cipucrs.pucrs.br>
2019-06-04 18:53:49 -0300

    Exercicios Lab02

--- TypeScriptNode_Configuracao/src/index.ts
+++ TypeScriptNode_Configuracao/src/index.ts
@@ -1,1 +1,33 @@
 console.log('Olá, mundo!'); 
+
+//Exercicio 1
+function pares(inicio: number, fim: number) {
+    let par = [];
+    for(let i = inicio; i<=fim; i++ ) {
+        if (i%2 === 0) {
+            par.push(i);
+        }
+    }
+    return par;
+}
+console.log(pares(1,10));
+function pares2(inicio: number, fim: number) {
+    let par = [];
+    let i = inicio;
+    while(i<=fim) {
+        if (i%2 === 0) {
+            par.push(i);
+        }
+        i++;
+    }
+    return par;
+}
+console.log(pares2(5,15));
+
+//Exercicio 2
+let i: number = 0;
+while (i != 10) {
+    i += 0.2;
+}
+
+
