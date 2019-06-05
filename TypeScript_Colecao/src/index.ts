4f5b1725f7b00f2997b41f01857aa0d6a86acca4
= <eduagost99@gmail.com>
2019-06-05 17:15:13 -0300

    colecao

---
+++ TypeScript_Colecao/src/index.ts
@@ -0,0 +1,20 @@
+let mapa : Map <string, string>;
+mapa = new Map();
+mapa.set('RS', 'Rio Grande do Sul');
+mapa.set('SC', 'Santa Catarina');
+
+console.log(mapa.size);
+console.log(mapa.get('RJ'));
+
+for (let entrada of mapa) {
+    console.log(entrada[0]);
+    console.log(entrada[1]);
+}
+
+for (let sigla of mapa.keys()) {
+    console.log(sigla);
+}
+
+for (let estados of mapa.values()) {
+    console.log(estados);
+}
