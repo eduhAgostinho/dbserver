985bfe00d388b0597da6dc53e750a0090c0ee7a5
Aluno <Aluno@cipucrs.pucrs.br>
2019-06-07 19:40:28 -0300

    exercicio lab03

--- atividadesLab03/src/index.ts
+++ atividadesLab03/src/index.ts
@@ -59,6 +59,10 @@ class Cofrinho {
         this.moedas.push(m);
     }
 
+    get moeda(): Moeda[] {
+        return this.moedas;
+    }
+
     calcularTotal(): number {
         // let total = 0;
         // for(let i of this.moedas) {
@@ -68,14 +72,43 @@ class Cofrinho {
         const somador: (x: number, y: Moeda) => number = (soma,moeda) => soma + moeda.valor;
         return this.moedas.reduce(somador, 0);
     }
+
+    menorMoeda(): Moeda {
+        let reducer = (accumulator: Moeda, currentValue: Moeda) => currentValue.valor<accumulator.valor?accumulator=currentValue:accumulator;
+        return this.moedas.reduce(reducer);
+    }
+
+    menorValor(): number {
+        let reducer = (accumulator: Moeda, currentValue: Moeda) => currentValue.valor<accumulator.valor?accumulator=currentValue:accumulator;
+        return this.moedas.reduce(reducer).valor;
+    }
+
+    freqMoedas(array: Moeda[]): Map<number,number> {
+        let mapa = new Map<number,number>();
+        mapa.set(1, 0);
+        mapa.set(5, 0);
+        mapa.set(10, 0);
+        mapa.set(25, 0);
+        mapa.set(50, 0);
+        mapa.set(100, 0);
+        // array.reduce((contagem, valor) => mapa.set(valor.valor, valor.valor);
+        // array.reduce((contagem, valor) => contagem.set(valor, (contagem.get(valor)||0)+1), new Map<number,number>());
+        return mapa;
+    }
+    
 }
-let moeda1: Moeda = new Moeda(50, "50 centavos");
-let moeda2: Moeda = new Moeda(25, "25 centavos");
+let moeda1: Moeda = new Moeda(5, "5 centavos");
+let moeda2: Moeda = new Moeda(100, "100 centavos");
+let moeda3: Moeda = new Moeda(10, "10 centavos");
 let cofre: Cofrinho = new Cofrinho();
 cofre.adicionar(moeda1);
 cofre.adicionar(moeda2);
+cofre.adicionar(moeda3);
 console.log(cofre.calcularTotal());
 
 // ----------------------------------------------------------------------------------------------
 
 // Exercício 3
+console.log(cofre.menorValor());
+console.log(cofre.menorMoeda());
+// console.log(cofre.freqMoedas());
