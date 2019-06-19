450769cfce13a0220d1040a6a6cfcfe4d5a5e67b
= <eduagost99@gmail.com>
2019-06-19 17:20:31 -0300

    aula 19/06

--- TypeScript_Express/src/index.ts
+++ TypeScript_Express/src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(consoleLogger);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
     res.send('Alô, Mundo!');
+});
+
+app.get('/:nome', (req,res) => {
+    res.send(`Alô ${req.params.nome}`);
+});
+
+app.post('/', (req,res) => {
+    const pessoa = req.body as {nome: string};
+    res.send(`Alô ${pessoa.nome}`);
 });
 
 app.listen(3000, () => {console.log('Rodando na porta 3000...')});
